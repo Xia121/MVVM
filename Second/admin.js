8104f40d45dbfa707b8927093beee385e5155d38
Xia121 <1751650135@qq.com>
2019-06-21 14:23:27 +0800

    MVC设计模式化

--- Second/admin.js
+++ Second/admin.js
@@ -1,82 +1,93 @@
-
-axios.interceptors.response.use(function (response) {
-    return response
-})
-
-
-function fetchDb() {
-  return axios.get('http://127.0.0.1:3002/api')
+let model = {
+    data: {
+        number: 0,
+        name: ''
+    },
+    fetch(id) {
+        return axios.get('http://127.0.0.1:3002/api').then(({data}) => {
+            this.data = data
+        })
+    },
+    update(newData) {
+        return axios.put('http://127.0.0.1:3002/put', newData).then(({data}) => {
+            this.data = data
+        })
+    }
 }
 
-function saveDb(newData) {
-  return axios.put('http://127.0.0.1:3002/put', newData)
+let view = {
+    el: '#app',
+    template: `
+    <div>
+      书名：《__name__》，
+      数量：<span id="number">__number__</span>
+    </div>
+    <div class="actions">
+      <button id="increaseByOne">加1</button>
+      <button id="decreaseByOne">减1</button>
+      <button id="square">平方</button>
+      <button id="cube">立方</button>
+      <button id="reset">归零</button>
+    </div>
+    `,
+    render (data) {
+        let html = this.template.replace('__name__', data.name)
+            .replace('__number__',data.number);
+
+        $(this.el).html(html)
+    }
 }
 
-var template = `
-<div>
-  书名：《__name__》，
-  数量：<span id="number">__number__</span>
-</div>
-<div class="actions">
-  <button id="increaseByOne">加1</button>
-  <button id="decreaseByOne">减1</button>
-  <button id="square">平方</button>
-  <button id="cube">立方</button>
-  <button id="reset">归零</button>
-</div>
-`
-
-
-fetchDb().then((request) => {
-  let result = request.data
-
-  $('#app').html(
-    template.replace('__number__', result.number)
-      .replace('__name__', result.name)
-  )
-
-  //加1
-  $('#increaseByOne').on('click', (e) => {
-    let oldResult = parseInt($('#number').text(), 10)
-    let newResult = oldResult + 1
-    saveDb({number: newResult}).then(function({data}) {
-        console.log(data)
-      $('#number').text(data.number)
-    })
-  })
-
-  //减1
-  $('#decreaseByOne').on('click', (e) => {
-    let oldResult = parseInt($('#number').text(), 10)
-    let newResult = oldResult - 1
-    saveDb({number: newResult}).then(({data}) => {
-      $('#number').text(data.number)
-    })
-  })
-
-  //平方
-  $('#square').on('click', (e) => {
-    let oldResult = parseInt($('#number').text(), 10)
-    let newResult = Math.pow(oldResult, 2)
-    saveDb({number: newResult}).then(({data}) => {
-      $('#number').text(data.number)
-    })
-  })
-
-  //立方
-  $('#cube').on('click', (e) => {
-    let oldResult = parseInt($('#number').text(), 10)
-    let newResult = Math.pow(oldResult, 3)
-    saveDb({number: newResult}).then(({data}) => {
-      $('#number').text(data.number)
-    })
-  })
-
-  //归零
-  $('#reset').on('click', (e) => {
-    let newResult = 0
-    saveDb({number: newResult}).then(({data}) => {
-      $('#number').text(data.number)
-    })
-  })
-})
+let controller = {
+    init ({view, model}) {
+        this.view = view;
+        this.model = model;
+        this.view.render(this.model.data);
+        this.bindEvent();
+        this.fetchModel()
+    },
+    events: [
+        { type: 'click', selector: '#increaseByOne', fnName: 'add' },
+        { type: 'click', selector: '#decreaseByOne', fnName: 'minus' },
+        { type: 'click', selector: '#square', fnName: 'square' },
+        { type: 'click', selector: '#cube', fnName: 'cube' },
+        { type: 'click', selector: '#reset', fnName: 'zero' },
+    ],
+    bindEvent () {
+        this.events.map((event) => {
+            $(this.view.el).on(event.type, event.selector, this[event.fnName].bind(this))
+        })
+    },
+    add(){
+        let newData = {number: this.model.data.number + 1}
+        this.updateModel(newData)
+    },
+    minus(){
+        let newData = {number: this.model.data.number - 1}
+        this.updateModel(newData)
+    },
+    square(){
+        let newData = {number: Math.pow(this.model.data.number, 2)}
+        this.updateModel(newData)
+    },
+    cube(){
+        let newData = {number: Math.pow(this.model.data.number, 3)}
+        this.updateModel(newData)
+    },
+    zero () {
+        let newData = {number: 0}
+        this.updateModel(newData)
+    },
+    fetchModel(){
+        this.model.fetch(1).then(() => {
+            console.log(this.model.data)
+            this.view.render(this.model.data)
+        })
+    },
+    updateModel(newData) {
+        this.model.update(newData).then(()=>{
+            this.view.render(this.model.data)
+        })
+    }
+}
+controller.init({view, model})
